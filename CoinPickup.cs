f7bebd386ec128936c506ca22d87454e40f60b1b
DaSpanky <54967220+tstenoff@users.noreply.github.com>
2019-09-09 13:19:43 -0400

    Add files via upload

---
+++ CoinPickup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
